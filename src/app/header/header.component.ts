cdf16ce77fe279edaa8092b1d0bb9d28289018ad
danielwarui <waruindungu@gmail.com>
2018-01-31 01:18:43 +0300

    angular 2 student learning

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by danielwarui on 10/11/2017.
+ */
+import {Component} from '@angular/core';
+@Component({
+  selector : 'app-header',
+  templateUrl : './header.component.html'
+})
+export class  HeaderComponent {
+
+}
